d9f2926bd743b0e470ffa5f811790007bfe8f133
ebokai <ebokai@hotmail.com>
2025-10-22 17:35:31 +0200

    Refactor simulation to use Numba-accelerated matrix operations
    Replaces the Bacterium class and list-based simulation with a Numba-accelerated matrix-based approach for bacteria and antibiotic modeling. Removes the now-redundant antibiotic_resistance_refactor.py file and consolidates all logic into antibiotic_resistance.py, improving performance and maintainability.

--- antibiotic_resistance/antibiotic_resistance.py
+++ antibiotic_resistance/antibiotic_resistance.py
@@ -1,66 +1,82 @@
 import numpy as np 
+from tqdm import tqdm
 import matplotlib.pyplot as plt 
+from numba import njit, prange
 
 
-class Bacterium(object):
-    def __init__(self, i, j, MIC, n_mutations = 0):
+@njit()
+def reproduce_numba(bacteria_matrix, p_reproduce = 0.05, p_mutate = 0.01):
 
-        # location on lattice
-        self.i = i
-        self.j = j
-        
-        # minimum inhibitory concentration
-        self.MIC = MIC 
+    for i in range(simulation_size):
+        for j in range(simulation_size):
+            occupied = bacteria_matrix[i,j,0]
+            mic = bacteria_matrix[i,j,1]
+            n_mutations = bacteria_matrix[i,j,2]
+            if not occupied:
+                continue
 
-        # number of mutations
-        self.n_mutations = n_mutations
-    
+            new_i = (i + np.random.randint(-1,2)) % simulation_size
+            new_j = (j + np.random.randint(0,2)) % simulation_size
 
-def reproduce(bacteria, pairs, p_reproduce = 0.05, p_mutate = 0.01):
+            if not (new_i >= 0 and new_i < simulation_size and new_j >= 0 and new_j < simulation_size):
+                continue 
 
-    if len(bacteria) > 500:
-        sample = np.random.choice(np.arange(len(bacteria)), 500, replace = False)
-    else:
-        sample = np.arange(len(bacteria))
+            new_occupied = bacteria_matrix[new_i, new_j, 0]
 
-    for i in sample:
+            if not new_occupied and np.random.random() < p_reproduce:
+                bacteria_matrix[new_i, new_j, 0] = 1
 
-        b = bacteria[i]
-
-        if np.random.random() < p_reproduce:
-            new_i = (b.i + np.random.randint(-1,2)) % SIMSIZE
-            new_j = (b.j + np.random.randint(0,2)) % SIMSIZE
-            p1 = (new_i, new_j)
-            if p1 not in pairs:
                 if np.random.random() < p_mutate:
-                    new_MIC = b.MIC * 10 
-                    new_n_mutations = b.n_mutations + 1
-                else:
-                    new_MIC = b.MIC 
-                    new_n_mutations = b.n_mutations
-                bacteria.append(Bacterium(new_i, new_j, new_MIC, new_n_mutations))
-                pairs.append(p1)    
-    return bacteria, pairs
+                    mic *= np.random.uniform(1.5, 3.0)
+                    n_mutations += 1
+                    
+                bacteria_matrix[new_i, new_j, 1] = mic
+                bacteria_matrix[new_i, new_j, 2] = n_mutations
 
-def kill(bacteria, pairs, antibiotic_matrix):
-    for b in bacteria:
-        if b.MIC < antibiotic_matrix[b.i,b.j]:
-            bacteria.remove(b)
-            pairs.remove((b.i,b.j))     
-    return bacteria,pairs
+    return bacteria_matrix
+
+@njit(parallel=True)
+def kill_numba(bacteria_matrix, antibiotic_matrix):
+    for i in prange(simulation_size):
+        for j in prange(simulation_size):
+            occupied = bacteria_matrix[i,j,0]
+            mic = bacteria_matrix[i,j,1]
+            if not occupied:
+                continue
+            if mic < antibiotic_matrix[i,j]:
+                bacteria_matrix[i,j,0] = 0
+                bacteria_matrix[i,j,1] = 0
+                bacteria_matrix[i,j,2] = 0
+    return bacteria_matrix
+
+
+
 
 
 def initialize_ab_matrix(regions = 10, ab0 = 1):
-    antibiotic_matrix = np.zeros((SIMSIZE, SIMSIZE))
-    x = np.arange(SIMSIZE)
-    for i in range(SIMSIZE):
+    antibiotic_matrix = np.zeros((simulation_size, simulation_size))
+    x = np.arange(simulation_size)
+    for i in range(simulation_size):
         antibiotic_matrix[i,:] = ab0 * np.exp((x-10)/10)
     return antibiotic_matrix
 
 def initialize_bacteria(n):
+    if n > simulation_size:
+        n = simulation_size
+
+    bacteria_matrix = np.zeros((simulation_size, simulation_size, 3), dtype = float)
+
+    i = np.random.choice(np.arange(simulation_size), n, replace = False)
+    bacteria_matrix[i,0,0] = 1
+    bacteria_matrix[i,0,1] = 1
+    bacteria_matrix[i,0,2] = 0
+
+    return bacteria_matrix
+
+
     pairs, bacteria = [], []
     for _ in range(n):
-        i = np.random.randint(0, SIMSIZE)
+        i = np.random.randint(0, simulation_size)
         pair = (i,0)
         if pair not in pairs:
             pairs.append(pair)
@@ -70,26 +86,36 @@ def initialize_bacteria(n):
 
 
 # --- INITIALIZE ---
-SIMSIZE = 150
+simulation_size = 150
+iterations = 10000
+
+bacteria_matrix = initialize_bacteria(50)
 antibiotic_matrix = initialize_ab_matrix()
-pairs, bacteria = initialize_bacteria(50)
-# ------------------
+
+print(np.sum(bacteria_matrix[:,:,0] > 0) )
+
+# bacteria_matrix = reproduce_numba(bacteria_matrix)
+# n_bacteria = sum(bacteria_matrix[:,:,0] > 0) 
+# print(f'time: {t}, number of bacteria: {n_bacteria}')
+# # ------------------
 
             
 # --- SIMULATION ---
-for t in range(10000):
-    bacteria, pairs = reproduce(bacteria, pairs)
-    bacteria, pairs = kill(bacteria, pairs, antibiotic_matrix)
-    if t % 500 == 0:
-        print('time: %d, number of bacteria: %d' % (t,len(bacteria)))
+for t in tqdm(range(iterations)):
+    bacteria_matrix = reproduce_numba(bacteria_matrix)
+    bacteria_matrix = kill_numba(bacteria_matrix, antibiotic_matrix)
+    
 # ------------------
+
+i_list, j_list = np.where(bacteria_matrix[:,:,0] > 0)
+c_list = bacteria_matrix[i_list, j_list, 2]
 
 
 # --- PLOT ---
 plt.matshow(np.log10(antibiotic_matrix),fignum=0,cmap='gray_r')
 plt.colorbar(label='antibiotic concentration [log mg/L]')
-plt.scatter([b.j for b in bacteria],[b.i for b in bacteria],
-    c=[b.n_mutations for b in bacteria], cmap='autumn', edgecolor='None', s = 10)
+plt.scatter(j_list, i_list,
+    c=c_list, cmap='autumn', edgecolor='None', s = 10)
 plt.colorbar(label = 'number of mutations')
 plt.show()
 # ------------------
